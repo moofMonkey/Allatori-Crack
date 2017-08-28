10a62e95d813eeffb50aca823f61f30231521136
Nikita <tyumencev2004@bk.ru>
2017-08-28 16:38:28 +0300

    Update ClassNameGen.java

--- net/moofMonkey/eye/modules/ClassNameGen.java
+++ net/moofMonkey/eye/modules/ClassNameGen.java
@@ -3,14 +3,14 @@ package net.moofMonkey.eye.modules;
 import java.security.SecureRandom;
 
 public class ClassNameGen extends Thread {
-	public static String alphabet = "Ii";
+	public static char[] alphabet = new char[] { 'I', 'i' };
 	private static SecureRandom rand = new SecureRandom();
 
 	public static String get(int len) {
 		char[] name = new char[len];
 
 		for(int i = 0; i < len; i++)
-			name[i] = alphabet.charAt(rand.nextInt(alphabet.length()));
+			name[i] = alphabet[rand.nextInt(alphabet.length)];
 
 		return new String(name);
 	}

adbcd0e8eb2504e928229d17555438550df84463
Nikita <tyumencev2004@bk.ru>
2017-08-28 15:58:04 +0300

    Fixed memory and CPU overusage

--- net/moofMonkey/eye/modules/ClassNameGen.java
+++ net/moofMonkey/eye/modules/ClassNameGen.java
@@ -1,28 +1,17 @@
 package net.moofMonkey.eye.modules;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class ClassNameGen extends Thread {
 	public static String alphabet = "Ii";
+	private static SecureRandom rand = new SecureRandom();
 
-	public static String get(int c) {
-		String name = "";
+	public static String get(int len) {
+		char[] name = new char[len];
 
-		try {
-			int i = c;
-			Random rand = new Random();
-			while (true) {
-				if (i == 0)
-					break;
-				name += alphabet.charAt(rand.nextInt(alphabet.length()));
-				i--;
-			}
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-		
-		System.gc();
+		for(int i = 0; i < len; i++)
+			name[i] = alphabet.charAt(rand.nextInt(alphabet.length()));
 
-		return name;
+		return new String(name);
 	}
 }
